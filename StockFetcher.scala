f03b3d94173b4b0da6daaceaca82361700caba05
Pascal Bugnion <pascal@bugnion.org>
2016-02-15 07:43:54 +0000

    Use scalaj-http to query markit on demand API.
    Previously used Source.fromURL. This improves consistency across the code and offers a slightly nicer interface since we don't need to construct the query string manually.

--- StockFetcher.scala
+++ StockFetcher.scala
@@ -1,22 +1,24 @@
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.io.Source
 import scala.xml.XML
+
+import scalaj.http.{Http, HttpRequest}
 
 trait StockFetcher {
 
   def stockSymbol: String
 
   def stockPrice: Future[BigDecimal] = {
-    val strResponse = Future { Source.fromURL(url).mkString }
-    val xmlResponse = strResponse.map { s => XML.loadString(s) }
-    val price = xmlResponse.map { r => BigDecimal((r \ "LastPrice").text) }
+    val strResponse = Future { request.asString }
+    val xmlResponse = strResponse.map { s => XML.loadString(s.body) }
+    val priceAsString = xmlResponse.map { r =>  (r \ "LastPrice").text }
+    val price = priceAsString.map { price => BigDecimal(price) }
     price
   }
 
-  private def url:String =
-    "http://dev.markitondemand.com/MODApis/Api/v2/Quote?" +
-    s"symbol=${stockSymbol}"
+  private def request:HttpRequest =
+    Http("http://dev.markitondemand.com/MODApis/Api/v2/Quote")
+      .param("symbol", stockSymbol)
 
 }
